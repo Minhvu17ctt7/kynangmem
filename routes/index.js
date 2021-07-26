7997d5fec7c3435f9925352220eda53761951c42
Minhvu17ctt7 <Minhvu1712918@gmail.com>
2021-07-26 21:56:11 +0700

    fisrt commit

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,27 @@
 var express = require("express");
 var router = express.Router();
-var upload = require("../multer/multer")
+var upload = require("../multer/multer");
 const mongoModel = require("../db/model");
-
 
 /* GET home page. */
 router.get("/", async (req, res, next) => {
   if (req.query.mssv) {
     console.log(req.query);
     const profile = await mongoModel.findOne(req.query);
-    const mssvs = await mongoModel.find({});
-    res.render("profile", { profile, mssvs });
+    const list = await mongoModel.find({});
+    res.render("profile", { profile, list });
   } else next();
 });
 router.get("/", async function (req, res, next) {
-  const mssvs = await mongoModel.find({});
-  res.render("index", { title: "Profile", mssvs });
+  const list = await mongoModel.find({});
+  res.render("index", { title: "Profile", list });
 });
 router.get("/profile/create", async (req, res, next) => {
   res.render("createprofile");
 });
-router.post("/profile/create", upload.single('image'),(req, res) => {
-  // const {name, email, skype,phone, age,mssv} = req.body;
-  ;
+router.post("/profile/create", upload.single("image"), (req, res) => {
   req.body.image = req.file.filename;
-  console.log(req.body)
+  console.log(req.body);
   mongoModel.create(req.body);
   res.redirect("/?mssv=" + req.body.mssv);
 });
